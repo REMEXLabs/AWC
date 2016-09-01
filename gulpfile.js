4b41df7c5d6f362b12c8be37d4446cc5f3c55825
nok <darius.morawiec@nok.onl>
2016-09-01 15:54:06 +0200

    add testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
-// const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const jasmine = require('gulp-jasmine');
+const reporters = require('jasmine-reporters');
+const util = require('gulp-util');
 
-gulp.task('transpile', () =>
-  gulp.src('src/*.js')
-  .pipe(sourcemaps.init())
-  .pipe(babel({
-    presets: ['es2015']
-  }))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('lib'))
-);
+gulp.task('build', function () {
+  return gulp.src('src/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('lib'));
+});
 
-gulp.task('default', ['transpile']);
+gulp.task('test', ['build'], function () {
+  return gulp.src(['spec/*.js'])
+    .pipe(jasmine({
+      verbose: false
+    }).on('error', util.log));
+});
+
+gulp.task('default', ['build', 'test']);
